969411c523b916dcc7188807d0d9a7f790fce4ce
Aqsa <aqsa@onebyte.biz>
2018-11-05 16:07:54 +0500

    commit by aqsa

---
+++ customlibmodule/src/main/java/com/example/customlibmodule/CustomTexts.java
@@ -0,0 +1,12 @@
+package com.example.customlibmodule;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class CustomTexts {
+public static void changeText(TextView textView,String text ){
+    textView.setText(text);
+}
+}
